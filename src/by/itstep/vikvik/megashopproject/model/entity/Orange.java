b3597f57797dc9b23442976a70300f3bf879638e
ivanvikvik <ivanvikvik@gmail.com>
2022-04-27 19:42:41 +0300

    Initial commit

---
+++ src/by/itstep/vikvik/megashopproject/model/entity/Orange.java
@@ -0,0 +1,41 @@
+package by.itstep.vikvik.megashopproject.model.entity;
+
+public class Orange {
+    private int diameter;
+    private int vitaminC;
+    private double cost;
+
+    public Orange(){
+
+    }
+
+    public Orange(int diameter, int vitaminC, double cost) {
+        this.diameter = diameter;
+        this.vitaminC = vitaminC;
+        this.cost = cost;
+    }
+
+    public int getDiameter() {
+        return diameter;
+    }
+
+    public void setDiameter(int diameter) {
+        this.diameter = diameter;
+    }
+
+    public int getVitaminC() {
+        return vitaminC;
+    }
+
+    public void setVitaminC(int vitaminC) {
+        this.vitaminC = vitaminC;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+}
