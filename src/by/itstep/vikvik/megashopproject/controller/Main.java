25fb5588ad82480d166cee86a65266a668e6359d
ivanvikvik <ivanvikvik@gmail.com>
2022-04-27 21:05:02 +0300

    Add Basket class

--- src/by/itstep/vikvik/megashopproject/controller/Main.java
+++ src/by/itstep/vikvik/megashopproject/controller/Main.java
@@ -2,11 +2,24 @@ package by.itstep.vikvik.megashopproject.controller;
 
 import by.itstep.vikvik.megashopproject.model.entity.Milk;
 import by.itstep.vikvik.megashopproject.model.entity.Orange;
+import by.itstep.vikvik.megashopproject.model.entity.container.Basket;
 
 public class Main {
     public static void main(String[] args) {
         Orange orange = new Orange(200, 3000, 4.5);
-        Milk milk = new Milk(1000, 3.5, 2.5);
-        System.out.println(milk);
+        Milk milk1 = new Milk(1000, 3.5, 2.5);
+        Milk milk2 = new Milk(1000, 3.5, 2.5);
+        Milk milk3 = new Milk(1000, 3.5, 2.5);
+        Milk milk4 = new Milk(1000, 3.5, 2.5);
+
+
+        Basket basket = new Basket(null, null);
+        basket.add((Milk)null);
+        basket.add(milk2);
+        basket.add(milk3);
+        basket.add(milk4);
+        basket.add(orange);
+
+        System.out.println(basket);
     }
 }
---
+++ src/by/itstep/vikvik/megashopproject/model/entity/container/Basket.java
@@ -0,0 +1,109 @@
+package by.itstep.vikvik.megashopproject.model.entity.container;
+
+import by.itstep.vikvik.megashopproject.model.entity.Milk;
+import by.itstep.vikvik.megashopproject.model.entity.Orange;
+
+import java.util.Arrays;
+
+public class Basket {
+    private Orange[] oranges;
+    private Milk[] milks;
+
+    public Basket(){
+        oranges = new Orange[0];
+        milks = new Milk[0];
+    }
+
+    public Basket(Orange[] oranges, Milk[] milks){
+        this();
+
+        if (oranges != null) {
+            this.oranges = oranges;
+        }
+
+        if (milks != null) {
+           this.milks = milks;
+        }
+    }
+
+    public void add(Orange orange){
+        if (orange == null) {
+            return;
+        }
+
+        Orange[] temp = new Orange[oranges.length + 1];
+
+        int i = 0;
+        for (; i < oranges.length; i++) {
+            temp[i] = oranges[i];
+        }
+
+        temp[i] = orange;
+        oranges = temp;
+    }
+
+    public void removeOrange(int index){
+        Orange[] temp = new Orange[oranges.length - 1];
+
+        for (int i = 0, j = 0; i < oranges.length; i++) {
+            if (i != index) {
+                temp[j] = oranges[i];
+                j++;
+            }
+        }
+
+        oranges = temp;
+    }
+
+    public void add(Milk milk){
+        if (milk == null) {
+            return;
+        }
+
+        Milk[] temp = new Milk[milks.length + 1];
+
+        int i = 0;
+        for (; i < milks.length; i++) {
+            temp[i] = milks[i];
+        }
+
+        temp[i] = milk;
+        milks = temp;
+    }
+
+    public void removeMilk(int index){
+        Milk[] temp = new Milk[milks.length - 1];
+
+        for (int i = 0, j = 0; i < milks.length; i++) {
+            if (i != index) {
+                temp[j] = milks[i];
+                j++;
+            }
+        }
+
+        milks = temp;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+
+        if (oranges == null || milks == null
+                || (milks.length == 0 && oranges.length == 0)) {
+            builder.append("Basket is empty.");
+        } else {
+            builder.append("Basket:");
+
+            for (Orange orange: oranges) {
+                builder.append("\n").append(orange);
+            }
+
+            for (Milk milk: milks) {
+                builder.append("\n").append(milk);
+            }
+        }
+
+
+        return builder.toString();
+    }
+}
