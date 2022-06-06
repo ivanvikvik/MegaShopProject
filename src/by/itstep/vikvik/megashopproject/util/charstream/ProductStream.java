c99c7d18c9e96e0713d885ce3a12b882a70be6aa
ivanvikvik <ivanvikvik@gmail.com>
2022-06-06 20:54:29 +0300

    Using character streams for parsing data without marker

--- src/by/itstep/vikvik/megashopproject/util/charstream/ProductStream.java
+++ src/by/itstep/vikvik/megashopproject/util/charstream/ProductStream.java
@@ -11,13 +11,12 @@ public class ProductStream {
         Writer writer = null;
 
 
-
         try {
             writer = new BufferedWriter(new FileWriter(fileName));
 
-            writer.write(oranges.length +"\n");
+            // writer.write(oranges.length +"\n");
 
-            for (Orange orange:oranges) {
+            for (Orange orange : oranges) {
                 writer.write(orange.getPrice() + " ");
                 writer.write(orange.getVitaminC() + " ");
                 writer.write(orange.getDiameter() + "\n");
@@ -44,12 +43,12 @@ public class ProductStream {
         try {
             reader = new BufferedReader(new FileReader(fileName));
 
-            int size = Integer.parseInt(reader.readLine());
+            //int size = Integer.parseInt(reader.readLine());
 
+            String buf = reader.readLine();
 
-           //while(buf != null){
-            for (int i = 0; i < size; i++) {
-                String buf = reader.readLine();
+            while (buf != null && buf != "") {
+                //for (int i = 0; i < size; i++) {
                 String[] parameters = buf.split(" "); //"3.5 2500 200".split() -> String[] "3.5" "2500" "200"
                 Orange orange = new Orange();
                 double price = Double.parseDouble(parameters[0]);
@@ -59,7 +58,7 @@ public class ProductStream {
                 orange.setVitaminC(vitamin);
                 orange.setDiameter(diameter);
                 list.add(orange);
-             //   buf = reader.readLine();
+                buf = reader.readLine();
             }
 
         } catch (IOException exception) {
